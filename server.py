fa14e94ede9199895f07bb93b341f3ee22f777bf
Developer <dev@example.com>
2026-09-09 18:20:21 +0700

    Auto-sync update: 2026-09-09 18:20:20

--- server.py
+++ server.py
@@ -27,7 +27,7 @@ if sys.platform == "win32":
 
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-PUBLIC_BASE_URL = os.getenv("PUBLIC_DASHBOARD_URL", "https://barrel-kodak-jane-residents.trycloudflare.com")
+PUBLIC_BASE_URL = os.getenv("PUBLIC_DASHBOARD_URL", "https://campaign-preventing-asylum-oakland.trycloudflare.com")
 GROUPS_FILE = "groups.json"
 HISTORY_FILE = "history.json"
 LAST_BROADCAST_FILE = "last_broadcast_sent.json"
